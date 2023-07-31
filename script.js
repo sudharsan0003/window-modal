d1401ea53d2b32ae78092b5d5b2f8762571381d6
sudharsan0003 <sanmarly04@gmail.com>
2023-07-31 17:49:24 +0530

    Responsive

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 'use strict';
 
-const btnShowModal = document.querySelectorAll('.btn-show');
+const btnShowModalOne = document.querySelector('.btn-show-one');
+const btnShowModalTwo = document.querySelector('.btn-show-two');
+console.log(btnShowModalTwo);
 // console.log(btnShowModal.length);
-const btnCloseModal = document.getElementById('btn-close');
+const btnCloseModalOne = document.getElementById('btn-close-one');
+const btnCloseModalTwo = document.getElementById('btn-close-two');
 
-const modalWindow = document.getElementById('modal');
+const modalWindowOne = document.getElementById('modal-one');
+const modalWindowTwo = document.getElementById('modal-two');
+
+btnShowModalOne.addEventListener('click', function () {
+  modalWindowOne.classList.remove('popup-one');
+});
+btnShowModalTwo.addEventListener('click', function () {
+  modalWindowTwo.classList.remove('popup-two');
+});
 
 // modalWindow.classList.add('modal-window');
 // // console.log(modalWindow.classList);
 // modalWindow.classList.remove('popup');
 // // console.log(modalWindow.classList);
 
-for (let i = 0; i < btnShowModal.length; i++) {
-  console.log(btnShowModal[i]);
-  btnShowModal[i].addEventListener('click', function () {
-    modalWindow.classList.remove('popup');
-  });
-}
-btnCloseModal.addEventListener('click', function () {
-  modalWindow.classList.add('popup');
+// for (let i = 0; i < btnShowModal.length; i++) {
+//   console.log(btnShowModal[i]);
+//   btnShowModal[i].addEventListener('click', function () {
+//     modalWindow.classList.remove('popup');
+//   });
+// }
+btnCloseModalOne.addEventListener('click', function () {
+  modalWindowOne.classList.add('popup-one');
+});
+btnCloseModalTwo.addEventListener('click', function () {
+  modalWindowTwo.classList.add('popup-two');
 });
 
 document.addEventListener('keydown', function (event) {
